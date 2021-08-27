b201c318ac66c7aff6e70a653dbf216c0678949b
Elizaveta <eli3avetta@mail.ru>
2021-08-27 19:22:58 +0300

    save

--- js/program-component.js
+++ js/program-component.js
@@ -2,12 +2,23 @@ class programComponent {
   hostElem;
   btsOpenDescription;
   descriptionElems;
+  btnsNavigate;
+  contentBlockElems;
+  btnsFilter;
+  contentFilterElems;
+
+  indexCurrentContent;
 
   constructor() {
     this.hostElem = document.querySelector('#program-host');
     if (!this.hostElem) return;
     this.btsOpenDescription = this.hostElem.querySelectorAll('.program__content-info-btn-show');
     this.descriptionElems = this.hostElem.querySelectorAll('.program__content-info-list');
+
+    this.btnsNavigate = this.hostElem.querySelectorAll('.program__navigate-btn');
+    this.contentBlockElems = this.hostElem.querySelectorAll('.program__bottom-block-wrapper');
+
+    this.btnsFilter = this.hostElem.querySelectorAll('.program__filter-btn');
 
     this.btsOpenDescription.forEach((btn, index) => {
       btn.onclick = () => {
@@ -18,6 +29,34 @@ class programComponent {
           } else {
             descriptionElem.classList.remove('mod-show');
             btn.innerText = 'Подробнее';
+          }
+        })
+      }
+    })
+
+    this.btnsNavigate.forEach((btn, index) => {
+      btn.onclick = () => {
+        this.indexCurrentContent = index;
+
+        this.contentBlockElems.forEach((contentBlock, i) => {
+          if (i === index) {
+            contentBlock.classList.add('mod-show');
+          } else {
+            contentBlock.classList.remove('mod-show');
+          }
+        })
+      }
+    })
+
+    this.btnsFilter.forEach(btn => {
+      btn.onclick = () => {
+        this.contentFilterElems = this.contentBlockElems[this.indexCurrentContent].querySelectorAll('.program__content-item');
+
+        this.contentFilterElems.forEach(elem => {
+          if (elem.className.includes(btn.value)) {
+            elem.classList.add('mod-show');
+          } else {
+            elem.classList.remove('mod-show');
           }
         })
       }
